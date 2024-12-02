c67e2908e46883dd9b9a87958d78d7228448bf39
Hermi-git <andargiehermela1@gmail.com>
2024-12-02 10:40:49 +0300

    Header and Footer

---
+++ netflix-clone/src/components/Footer/Footer.js
@@ -0,0 +1,60 @@
+import React from 'react'
+import FacebookOutlinedIcon from '@mui/icons-material/FacebookOutlined';
+import YouTubeIcon from '@mui/icons-material/YouTube';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import './footer.css';
+
+const Footer = () => {
+  return (
+    <div className='footer-outer-container'>
+      <div className="footer-inner-container">
+      <div className="footer-icons">
+        <FacebookOutlinedIcon/>
+        <YouTubeIcon/>
+        <InstagramIcon/>
+      </div>
+      <div className="footer-data">
+        <div>
+        <ul>
+        <li>Audio description</li>
+        <li>Investor Relations</li>
+        <li>Legal Notice</li>
+        </ul>
+      </div>
+
+      <div >
+        <ul>
+          <li>help center</li>
+          <li>jobs</li>
+          <li>cookie preference</li>
+        </ul>
+      </div>
+      <div>
+        <ul>
+          <li>Gift Cards</li>
+          <li>Terms of use</li>
+          <li>Corporate Information</li>
+        </ul>
+      </div>
+      <div >
+        <ul>
+          <li>Media center</li>
+          <li> Privacy</li>
+          <li>contact us</li>
+        </ul>
+      </div>
+    </div>
+    <div className='service-code'>
+      <p>
+        Service Code
+      </p>
+    </div>
+    
+    <div className="copy-writer">
+      &copy; 1997-2024 Netflix,Inc.
+    </div>
+    </div>
+    </div>
+  )
+}
+ export default Footer
